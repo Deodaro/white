ed6adbd94b01da8b4c20b54311654e1e86621183
Deodaro <l_r@list.ru>
2021-02-14 21:57:39 +1000

    Add slick-slider

--- js/main.js
+++ js/main.js
@@ -4,10 +4,20 @@
 // mwheelIntent.js - not included
 // jquery.jscrollpane.min.js
 
-$(function () {
-  $('.developers__gallery').jScrollPane();
-});
-
 // $(function () {
-//   $('.developers__carousel').jScrollPane();
+//   $('.developers__gallery').jScrollPane();
 // });
+
+$(document).ready(function () {
+  $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: document.querySelector('#slider-prev-btn'),
+    nextArrow: document.querySelector('#slider-next-btn'),
+    dots: true,
+    appendDots: document.querySelector('#slider-scrollbar'),
+    // dotsClass: document.querySelector('#slider-scrollbar'),
+  });
+});

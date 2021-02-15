128e56e85cb83d936daa9d0e62fdb8f0fa75ccad
Deodaro <l_r@list.ru>
2021-02-15 17:35:31 +1000

    Add little fixes to styles

--- js/main.js
+++ js/main.js
@@ -8,16 +8,48 @@
 //   $('.developers__gallery').jScrollPane();
 // });
 
-$(document).ready(function () {
-  $('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
+// $(document).ready(function () {
+//   $('.multiple-items').slick({
+//     infinite: true,
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     arrows: true,
+//     prevArrow: document.querySelector('#slider-prev-btn'),
+//     nextArrow: document.querySelector('#slider-next-btn'),
+//     // dots: true,
+//     // appendDots: document.querySelector('#slider-scrollbar'),
+//     // dotsClass: document.querySelector('#slider-scrollbar'),
+//   });
+// });
+
+const slidesCount = $('.slick-carousel').find('.slide-content').length,
+  $nav = $('.nav'),
+  $navInner = $nav.find('.nav-inner');
+
+function updateNav(slide) {
+  $navInner.width(`${($nav.width() * (slide + 1)) / (slidesCount - 3)}px`);
+}
+
+$('.nav').on('click', function (e) {
+  $('.slick-carousel').slick(
+    'slickGoTo',
+    ((e.offsetX / $nav.width()) * slidesCount) | 0
+  );
+});
+
+$('.slick-carousel')
+  .on({
+    init(e, slick) {
+      updateNav(slick.currentSlide);
+    },
+    beforeChange(e, slick, currentSlide, nextSlide) {
+      updateNav(nextSlide);
+    },
+  })
+  .slick({
     arrows: true,
+    slidesToShow: 4,
+    infinite: false,
     prevArrow: document.querySelector('#slider-prev-btn'),
     nextArrow: document.querySelector('#slider-next-btn'),
-    dots: true,
-    appendDots: document.querySelector('#slider-scrollbar'),
-    // dotsClass: document.querySelector('#slider-scrollbar'),
   });
-});
